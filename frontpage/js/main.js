813bbaaa44cf83e0f6a1e4c38c68c3f0a3161665
ludger <ludgerlef@hotmail.com>
2018-09-27 10:17:17 +0200

    Added date/username in the form

--- frontpage/js/main.js
+++ frontpage/js/main.js
@@ -55,4 +55,16 @@ $(function(){
     });
 
 
+    var d = new Date();
+
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+
+    var output =
+
+        ((''+day).length<2 ? '0' : '') + day + '/' +
+        ((''+month).length<2 ? '0' : '') + month + '/' +
+        d.getFullYear();
+    $(".date").append(output);
+
 });
