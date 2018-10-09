7f481afdd8425b6771451b1fe3ac2b956adce6b4
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-10-09 21:45:26 +0200

    Add voting javascript (doesn't do anything yet)

---
+++ frontpage/rateables/js/voting.js
@@ -0,0 +1,10 @@
+$(function() {
+	$(".vote-up").click(function() {
+		var review = $(this).attr("data-review-id");
+		// Vote up
+	});
+	$(".vote-down").click(function() {
+		var review = $(this).attr("data-review-id");
+		// Vote down
+	});
+});
