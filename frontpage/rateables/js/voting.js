b798898076e4d757426056859f5fb963d6947368
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-10-09 22:12:41 +0200

    Implement easier way to retrieve CSRF token

--- frontpage/rateables/js/voting.js
+++ frontpage/rateables/js/voting.js
@@ -1,20 +1,5 @@
 $(function() {
-	function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-    var csrftoken = getCookie('csrftoken');
+    var csrftoken = $("#csrf-token input").val();
 
 	$(".vote-up").click(function() {
 		var reviewId = $(this).attr("data-review-id");
