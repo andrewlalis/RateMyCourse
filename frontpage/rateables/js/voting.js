83c2390f729a8e4fe250e70297f47d127f699b2a
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-10-09 22:32:16 +0200

    Send POST request to imaginary endpoint for helpful votes

--- frontpage/rateables/js/voting.js
+++ frontpage/rateables/js/voting.js
@@ -3,10 +3,26 @@ $(function() {
 
 	$(".vote-up").click(function() {
 		var reviewId = $(this).attr("data-review-id");
+		var data = {
+			'csrfmiddlewaretoken': csrftoken,
+			'helpful': true
+		};
 		// Vote up
+		$.post(
+			'/api/postings/reviews/' + reviewId + '/helpful_vote',
+			data
+		);
 	});
 	$(".vote-down").click(function() {
 		var review = $(this).attr("data-review-id");
+		var data = {
+			'csrfmiddlewaretoken': csrftoken,
+			'helpful': false
+		};
 		// Vote down
+		$.post(
+			'/api/postings/reviews/' + reviewId + '/helpful_vote',
+			data
+		);
 	});
 });
